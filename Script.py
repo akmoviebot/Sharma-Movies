090464a743fd6b1aff9b82da075f2f5ac7851542
akmoviebot <mrak7753@gmail.com>
2025-01-21 19:02:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -248,7 +248,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<a href='https://t.me/Movieupdatewithak'>{file_name}</a>
 
 <b> Size ⚙️: {file_size}</b>""" 
 
