fe3f6563ea8fec6d38fd210986ee07b92977fd6c
akmoviebot <mrak7753@gmail.com>
2026-06-17 02:58:58 +0530

    Restore initial comments and import statements

--- info.py
+++ info.py
@@ -95,7 +95,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+TNl5DApmlz04Mzll')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MovieupdatewithAK0')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MovieupdatewithAK01')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MovieWalaClub') # Support Chat Link Without https:// or @
 
 
